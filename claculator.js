18baedff4e7cdb1dc80c83e5e51f045618c0053d
Devender-008 <126906072+Devender-008@users.noreply.github.com>
2024-04-10 14:01:52 +0530

    added the respective HTML, CSS and JavaScript files

---
+++ claculator.js
@@ -0,0 +1,19 @@
+var ClickSound1=new Audio()
+var ClickSound2=new Audio()
+var ClickSound3=new Audio()
+
+ClickSound1.src="./i1.mp3"
+ClickSound2.src="./m1.mp3"
+ClickSound3.src="./w1.mp3"
+function ClickAudio()
+{
+    ClickSound1.play()
+}
+function ClickAudio1()
+{
+    ClickSound2.play()
+}
+function ClickAudio2()
+{
+    ClickSound3.play()
+}
